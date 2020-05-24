23fafb41d82399eea58fb5ca4ef512fa4e8bc1df
Skidan <dmytro.skydanenko@gmail.com>
2020-05-24 22:10:16 +0300

    modifying frontend

---
+++ 04_program/Frontend_GULP/public/js/scripts.js
@@ -0,0 +1,13 @@
+// import { objectExpression } from "babel-types";
+console.log("Connected");
+
+////////// DOM variables //////////
+var 
+  domVar, 
+  plateVar, 
+  currentTrailer, 
+  umegaTrailers, 
+  activator;
+// Inputed data
+domVar = {};
+// Plate hooks
