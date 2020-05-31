b0199b337959ff47b2f73e42b8395aaa1ffcdff8
Skidan <dmytro.skydanenko@gmail.com>
2020-05-31 17:23:06 +0300

    to station

---
+++ 04_program/Qualify/Qualify/Domain/Entities/EntityBase.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Qualify.Domain.Entities
+{
+    public abstract class EntityBase
+    {
+        [Required]
+        public Guid ID { get; set; }
+
+        [Display(Name = "Name (heading)")]
+        public virtual string Title { get; set; }
+
+        [Display(Name = "Name (heading)")]
+        public virtual string Title { get; set; }
+
+        [Display(Name = "Name (heading)")]
+        public virtual string Title { get; set; }
+
+        [Display(Name = "Name (heading)")]
+        public virtual string Title { get; set; }
+
+        [Display(Name = "Name (heading)")]
+        public virtual string Title { get; set; }
+    }
+}
